23a81e6d47ea1cab0f33bc51b2b12fc62b2834ec
millers4245 <millers4245@masseyhigh.school.nz>
2025-08-01 12:14:51 +1200

    Add files via upload

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# functions here
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Main routine
+statement_generator("Instructions", "-")
