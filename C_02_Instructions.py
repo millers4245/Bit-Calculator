6d94fa9533bfbd7ad723d7103fdeb751144a386f
millers4245 <millers4245@masseyhigh.school.nz>
2025-08-05 14:40:23 +1200

    Add files via upload

---
+++ C_02_Instructions.py
@@ -0,0 +1,27 @@
+# Generates headings ( ----- Heading ---- )
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Displays instructions
+def instructions():
+    statement_generator("Instructions", "-")
+
+    print('''
+Instructions go here.
+- instruction 1
+- instruction 2
+- etc
+    ''')
+
+
+# Main routine
+
+# Display instructions if requested
+want_instructions = input("Press <enter> to read the instructions  "
+                          "or any key to continue")
+
+if want_instructions == "":
+    instructions()
+
+print("program continues")
