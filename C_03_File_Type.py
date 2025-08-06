07505abfc67386954bf0628231e8186fc40333d1
millers4245 <millers4245@masseyhigh.school.nz>
2025-08-07 09:40:45 +1200

    Add files via upload

---
+++ C_03_File_Type.py
@@ -0,0 +1,45 @@
+# asks users for file type (integer / image / text / xxx)
+def get_filetype():
+
+    while True:
+        response = input("File type: ").lower()
+
+         # check for 'i' or the exit code
+        if response =="xxx" or response == "i":
+            return response
+
+        # check if it's a integer
+        elif response in ['integer', 'int']:
+            return "integer"
+
+        # check for an image
+        elif response in ['image', 'picture', 'img', 'p']:
+            return "image"
+
+        # check  for text
+        elif response in ["text", 'txt', 't']:
+            return "text"
+
+        # if the response is invalid
+        else:
+            print("Please enter a valid file type")
+
+
+# Main routine goes here
+while True:
+    file_type = get_filetype()
+
+    # if user chose 'i' ask whether they want image / integer
+    if file_type == 'i':
+
+        want_image = input("Press <enter> for an integer or any key for an image. ")
+        if want_image == "":
+            file_type = "integer"
+        else:
+            file_type = "image"
+
+    print(f"You chose {file_type}")
+
+    if file_type == "xxx":
+        break
+
